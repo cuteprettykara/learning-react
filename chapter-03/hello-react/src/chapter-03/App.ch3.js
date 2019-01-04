b559a75cdcc6c237e61bb74e8af3993feef3e68c
cuteprettykara <cuteprettykara@gmail.com>
2019-01-04 12:30:39 +0900

    ch3: 컴포넌트

---
+++ chapter-03/hello-react/src/chapter-03/App.ch3.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './MyComponent'
+import MyComponent from './MyComponent';
+
+class App extends Component {
+  render() {
+   
+    return (
+      <MyComponent name="React" age={4}/>
+    );
+  }
+}
+
+export default App;
