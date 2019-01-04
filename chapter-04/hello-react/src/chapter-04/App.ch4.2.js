23dd40e3fc984d9997ef74073f0f82cc0c34571d
cuteprettykara <cuteprettykara@gmail.com>
2019-01-04 12:33:43 +0900

    ch4: 이벤트 핸들링

---
+++ chapter-04/hello-react/src/chapter-04/App.ch4.2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import EventPractice from './EventPractice.2.Named';
+
+class App extends Component {
+  render() {
+   
+    return (
+      <EventPractice />
+    );
+  }
+}
+
+export default App;
