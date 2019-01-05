f0db04377d26373ca63f0f55433e2565d671280c
cuteprettykara <cuteprettykara@gmail.com>
2019-01-05 12:10:43 +0900

    ch5: ref

---
+++ chapter-05/hello-react/src/chapter-04/App.ch4.4.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import EventPractice from './EventPractice.4.Multi-Input';
+
+class App extends Component {
+  render() {
+   
+    return (
+      <EventPractice />
+    );
+  }
+}
+
+export default App;
