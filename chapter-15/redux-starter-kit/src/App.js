274c649ad0b95b9574cb106e37fa04e54935e25a
cuteprettykara <cuteprettykara@gmail.com>
2019-02-01 14:48:31 +0900

    ch15: 리덕스 미들웨어와 외부 데이터 연동: redux-thunk - 웹요청처리 : redux-thunk와 axios 사용

--- chapter-15/redux-starter-kit/src/App.js
+++ chapter-15/redux-starter-kit/src/App.js
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
-import axios from 'axios';
+import * as postActions from './modules/post';
 
 class App extends Component {
+
+    loadData = () => {
+        const { PostActions, number } = this.props;
+        PostActions.getPost(number);
+    }
+
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/1')
-            .then(response => console.log(response));
+        this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.number !== prevProps.number) this.loadData();
     }
 
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, loading, error } = this.props;
 
         return (
             <div>
                 <h1>{number}</h1>
-                <button onClick={CounterActions.incrementAsync}>+</button>
-                <button onClick={CounterActions.decrementAsync}>-</button>
+                {
+                  loading
+                    ? <h2>로딩 중...</h2>
+                    : error
+                        ? <h2>오류 발생</h2>
+                        : <div>
+                            <h2>{post.title}</h2>
+                            <h2>{post.body}</h2>
+                          </div>
+                }
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
             </div>
         );
     }
@@ -25,9 +44,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
