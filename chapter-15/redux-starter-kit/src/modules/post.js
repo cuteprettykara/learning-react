074d68c84103393b782a612b2a988ef5239ad695
cuteprettykara <cuteprettykara@gmail.com>
2019-02-03 11:26:43 +0900

    ch15: 리덕스 미들웨어와 외부 데이터 연동: redux-pender - 웹요청처리 - 비동기작업이 여러 개일 때

--- chapter-15/redux-starter-kit/src/modules/post.js
+++ chapter-15/redux-starter-kit/src/modules/post.js
@@ -1,6 +1,6 @@
 import { handleActions, createAction } from 'redux-actions';
 import axios from 'axios';
-import { pender } from 'redux-pender';
+import { applyPenders } from 'redux-pender';
 
 function getPostAPI(postId) {
   return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
@@ -8,43 +8,38 @@ function getPostAPI(postId) {
 
 const GET_POST = 'GET_POST';
 
-/*
-  redux-pender의 액션 구조는 Flux standard action(https://github.com/acdlite/flux-standard-action)
-  을 따르기 때문에, createAction으로 액션을 만들 수 있습니다.
-  두 번째로 들어가는 파라미터는 Promise를 반환하는 함수여야 합니다.
-*/
 export const getPost = createAction(GET_POST, getPostAPI);
 
 const initialState = {
-  // 요청이 진행중인지, 오류가 발생하는지 여부는 더 이상 직접 관리할 필요가 없습니다.
-  // penderReducer가 담당하기 때문이죠.
   data: {
     title: '',
     body: ''
   }
 }
 
-export default handleActions({
-  ...pender({
-    type: GET_POST, // type이 주어지면 이 type에 접미사를 붙인 액션 핸들러들이 담긴 객체를 만듭니다.
+const reducer = handleActions({
+  // 다른 일반 액션들을 관리..
+}, initialState);
 
-/*  요청 중일 때와 실패했을 때 추가로 해야 할 작업이 있다면
-    이렇게 onPending과 onFailure를 추가하면 됩니다.
-      onPending: (state, action) => state,
-      onFailure: (state, action) =>  state
-*/
-      // 성공했을 때 해야 할 작업이 따로 없으면 이 함수 또한 생략해도 됩니다.
-      onSuccess: (state, action) => {
-        const { title, body } = action.payload.data;
-        return {
-          data: {
-            title,
-            body
-          }
+// 첫 번째 파라미터 : 일반 리듀서
+// 두 번재 파라미터 : pender 관련 객체들을 배열 형태로
+export default applyPenders(reducer, [
+  {
+    type: GET_POST,
+    // 성공했을 때 해야 할 작업이 따로 없으면 이 함수 또한 생략해도 됩니다.
+    onSuccess: (state, action) => {
+      const { title, body } = action.payload.data;
+      return {
+        data: {
+          title,
+          body
         }
       }
-
-      // 함수를 생략했을 때 기본 값으로는 (state, action) => state를 설정합니다.
-      // (state를 그대로 반환한다는 것이죠.)
-  })
-}, initialState);
+    }
+  },
+  /*
+    다른 pender 액션들
+    { type: GET_SOMETHING, onSuccess: (state, action) => ... },
+    { type: GET_SOMETHING, onSuccess: (state, action) => ... }
+  */
+]);
