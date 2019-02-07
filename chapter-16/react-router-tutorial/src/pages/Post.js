061a65de3491c21ebfabe446302b7c71ec97ab78
cuteprettykara <cuteprettykara@gmail.com>
2019-02-07 18:05:34 +0900

    ch16: react-router로 SPA개발: 라우트로 사용된 컴포넌트가 전달받는 props

--- chapter-16/react-router-tutorial/src/pages/Post.js
+++ chapter-16/react-router-tutorial/src/pages/Post.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Post = ({match}) => {
+const Post = ({match, location}) => {
+  console.log('location :', location);
+  console.log('Post :', match);
+
   return (
     <div>
       포스트 #{match.params.id}
