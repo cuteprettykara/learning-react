812fc56d4e6744261e3c749aef159252eb2105ec
cuteprettykara <cuteprettykara@gmail.com>
2019-02-07 11:17:30 +0900

    ch16: react-router로 SPA개발: 라우트 params 사용

--- chapter-16/react-router-tutorial/src/App.js
+++ chapter-16/react-router-tutorial/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div>
         <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
+        <Route path="/about/:name?/:anotherValue?" component={About} />
       </div>
     );
   }
