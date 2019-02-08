d7c526ac2720fa19606ad17891afc1192bef7a06
cuteprettykara <cuteprettykara@gmail.com>
2019-02-08 12:07:57 +0900

    ch18: Koa 프레임워크: Koa 기본 사용법 : 미들웨어

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,1 +1,29 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+// Promise를 이용하여 구현
+/* app.use((ctx, next) => {
+  console.log(1);
+  next().then(() => {
+    console.log('bye');
+  });
+}); */
+
+// async/await를 이용하여 구현
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log('bye');
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use(ctx => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => console.log('listening to port 4000'));
