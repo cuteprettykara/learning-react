6c2406e003eb9c3c0df57a1e73bd4f6cc5b9efaf
cuteprettykara <cuteprettykara@gmail.com>
2019-02-08 20:16:33 +0900

    ch18: Koa 프레임워크: koa-router 사용: 컨트롤러 파일 작성

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,12 +1,17 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const api = require('./api');
+const bodyParser = require('koa-bodyparser');
+
 
 const app = new Koa();
 const router = new Router();
 
 // 라우터 설정
 router.use('/api', api.routes());
+
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes())
