3e5704dd3920fa4b84a6c02170eed657914c9c34
cuteprettykara <cuteprettykara@gmail.com>
2019-02-12 12:12:18 +0900

    ch19: mongoose를 이용한 MongoDB 연동 실습: 페이지네이션 구현

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -57,9 +57,49 @@ exports.write = async ctx => {
   GET /api/posts
 */
 exports.list = async ctx => {
+  // page가 주어지지 않았다면 1로 간주
+  // query는 문자열 형태로 받아오므로 숫자로 변환
+  const page = parseInt(ctx.query.page || 1, 10);
+
+  // 잘못된 페이지가 주어졌다면 오류
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    // 내용 길이 제한 방법 1: toJSON 사용
+/*     const posts = await Post.find()
+      .sort({_id: -1})
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+
+    const limitBodyLength = post => ({
+      ...post.toJSON(),
+      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+    }); */
+
+    // 내용 길이 제한 방법 2: lean 사용
+    const posts = await Post.find()
+      .sort({_id: -1})
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+
+    const limitBodyLength = post => ({
+      ...post,
+      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+    });
+
+    ctx.body = posts.map(limitBodyLength);
+
+    // 마지막 페이지 알려주기
+    // ctx.set은 response header를 설정해줍니다.
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount/10));
+
   } catch (e) {
     ctx.throw(e, 500);
   }
