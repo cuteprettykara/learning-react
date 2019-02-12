28657886d105044b02793c21d4ea95c794101576
cuteprettykara <cuteprettykara@gmail.com>
2019-02-12 10:40:31 +0900

    ch19: mongoose를 이용한 MongoDB 연동 실습: 요청 검증

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,18 @@
 const Post = require('models/post');
+const { ObjectId } = require('mongoose').Types;
+const Joi = require('joi');
+
+exports.checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+
+  // 검증 실패
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;   // Bad Request
+    return null;
+  }
+
+  return next();  // next를 리턴해야 ctx.body가 제대로 설정됩니다.
+};
 
 /*
   포스트 작성
@@ -6,6 +20,23 @@ const Post = require('models/post');
   {title, body, tags}
 */
 exports.write = async ctx => {
+  // 객체가 지닌 값들을 검증합니다.
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),   // 뒤에 required를 붙여주면 필수 항목이라는 의미
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required()  // 문자열 배열
+  });
+
+  // 첫 번째 파라미터는 검증할 객체, 두 번째는 스키마
+  const result = Joi.validate(ctx.request.body, schema);
+
+  // 오류 발생 시 오류 내용 응답
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
 
   // 새 Post 인스턴스를 만듭니다.
