2e5ed598f8631b2962c73f542cf8cfc7c3b35d22
cuteprettykara <cuteprettykara@gmail.com>
2019-03-01 15:49:54 +0900

    ch20: 블로그 프로젝트: PostLIst 페이지 UI 구현

--- blog/blog-frontend/src/pages/ListPage.js
+++ blog/blog-frontend/src/pages/ListPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
+import ListWrapper from 'components/list/ListWrapper';
+import PostList from 'components/list/PostList';
+import Pagination from 'components/list/Pagination';
 
 const ListPage = () => {
   return (
     <div>
       <PageTemplate>
-        List
+        <ListWrapper>
+          <PostList />
+          <Pagination />
+        </ListWrapper>
       </PageTemplate>
     </div>
   );
