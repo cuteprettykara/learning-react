c3a63b0f0dc52aa137a811714dbde1327604caa2
cuteprettykara <cuteprettykara@gmail.com>
2019-03-04 10:22:11 +0900

    ch20: 블로그 프로젝트: Editor 페이지 UI 구현: EditorHeader, EditorPane, PreviewPane

---
+++ blog/blog-frontend/src/components/editor/PreviewPane/PreviewPane.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './PreviewPane.scss';
+import classNames from 'classnames';
+
+const cx = classNames.bind(styles);
+
+const PreviewPane = ({markdown, title}) => {
+  return (
+    <div className={cx('preview-pane')}>
+      <h1 className={cx('title')}>
+        제목
+      </h1>
+      <h1>
+        내용
+      </h1>
+    </div>
+  );
+};
+
+export default PreviewPane;
