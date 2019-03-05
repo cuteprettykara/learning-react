726bc2b2e79a218ed567fda2320f8bc53df255ee
cuteprettykara <cuteprettykara@gmail.com>
2019-03-05 11:36:32 +0900

    ch21: 프로젝트에서 API 연동: 포스트 작성

--- blog/blog-frontend/src/components/editor/EditorHeader/EditorHeader.js
+++ blog/blog-frontend/src/components/editor/EditorHeader/EditorHeader.js
@@ -9,7 +9,7 @@ const EditorHeader = ({onGoBack, onSubmit}) => {
   return (
     <div className={cx('editor-header')}>
       <div className={cx('back')}>
-        <Button onClick={onGoBack} theme="outline">뒤로하기</Button>
+        <Button onClick={onGoBack} theme="outline">뒤로가기</Button>
       </div>
       <div className={cx('submit')}>
         <Button onClick={onSubmit} theme="outline">작성하기</Button>
